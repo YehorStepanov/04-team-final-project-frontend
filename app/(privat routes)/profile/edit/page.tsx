edfa70301b2420965f216eed09acb128a8afeb8b
Oleh Yatsenko <mjp.kiev@gmail.com>
2026-01-11 17:38:04 +0200

    feat: add onboarding page in private routes

---
+++ app/(privat routes)/profile/edit/page.tsx
@@ -0,0 +1,8 @@
+export default function OnboardingPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Onboarding</h1>
+      <p>Onboarding page is under construction</p>
+    </main>
+  );
+}
